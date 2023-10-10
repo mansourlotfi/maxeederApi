00eeb45dc8c055cdc0df36f476420c29c382f044
mansourlotfi <mansourlotfi@gmail.com>
2023-10-10 13:16:24 +0330

    Update withOrigin

--- Program.cs
+++ Program.cs
@@ -172,7 +172,7 @@ if (environment.IsDevelopment())
     app.UseCors(opt =>
     {
         opt.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("http://localhost:3000");
-        opt.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("https://darkube.app");
+        opt.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("https://maxeeder-admin.darkube.app");
         opt.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("https://*.darkube.app")
                 .SetIsOriginAllowedToAllowWildcardSubdomains(); ;
 
