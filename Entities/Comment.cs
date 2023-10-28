8f33ca7a236bc2449d2202eb51cdec22cefd7bdd
mansourlotfi <mansourlotfi@gmail.com>
2023-10-28 23:12:21 +0330

    Add: sub category controller and comment controller

--- Entities/Comment.cs
+++ Entities/Comment.cs
@@ -7,7 +7,6 @@
         public string Email { get; set; }
         public string Text { get; set; }
         public bool IsActive { get; set; }
-        public DateTime CreationDate { get; set; }
 
     }
 }
